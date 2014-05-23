2a110cb4c764b7b56404f495b1eb2e07f9edd4b2
rosem <michael_rose@gmx.de>
2014-05-23 14:40:49 +0200

    version bump

--- dist/project/Build.scala
+++ dist/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName = "play4jpa"
-  val appVersion = "0.1-SNAPSHOT"
+  val appVersion = "0.2-SNAPSHOT"
 
   val appDependencies = Seq(
     javaCore,
